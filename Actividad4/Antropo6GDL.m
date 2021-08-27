08e7ebaffc042df3ac7044658f4fa832bd3e09d4
Rafael Islas <32578081+Rafael-Islas@users.noreply.github.com>
2021-08-27 14:25:38 -0500

    Los archivos buenos, finales 1

---
+++ Actividad4/Antropo6GDL.m
@@ -0,0 +1,67 @@
+%% path in r^3
+% as something
+% path=[-4.0000    1.0000    2.0000;
+%    -3.7050    2.2211    2.7050;
+%    -3.4100    2.4100    3.4100;
+%    -3.1150    2.2211    4.1150;
+%    -2.8200    1.0000    4.8200;
+%    -2.8200    1.0000    4.8200;
+%    -2.3500   -1.0000    5.0167;
+%    -1.8800   -1.4398    5.2133;
+%    -1.4100   -1.5563    5.4100;
+%    -0.9400   -1.5900    5.6067;
+%    -0.4700   -1.5563    5.8033;
+%          0    1.0000    6.0000;
+%          0    1.0000    6.0000;
+%     0.7050    1.5110    5.7050;
+%     1.4100    1.5900    5.4100;
+%     2.1150    1.5110    5.1150;
+%     2.8200    1.0000    4.8200;
+%     2.8200    1.0000    4.8200;
+%     3.0167   -1.0000    4.3500;
+%     3.2133   -2.0510    3.8800;
+%     3.4100   -2.3294    3.4100;
+%     3.6067   -2.4100    2.9400;
+%     3.8033   -2.3294    2.4700;
+%     4.0000    1.0000    2.0000];
+%% load('trayectoria.mat','txx')
+matObj = matfile('trayectoria.mat');
+path=matObj.txx;
+plot3(path(:,1), path(:,2), path(:,3), 'color', 'k', 'LineWidth', 2)
+xlabel('x');ylabel('y');zlabel('z') ;
+grid on
+%% Configuracion del robot
+%theta d a alpha sigma offset
+%l1a=7;l1b=2;l2=4;l3=4;l4=1.5;l5=1.5;l6=3;
+a1=5;a2=5;d1=7;d2=2;d3=1;
+L(1)=Link([0        d1	0	-pi/2   0]);
+L(2)=Link([0        d2	a1  0       0]);
+L(3)=Link([0        0	a2  0       0]);
+L(4)=Link([-pi/2    0	0	-pi/2   0]);
+L(5)=Link([pi/2     0	0	pi/2    0]);
+L(6)=Link([0        d3	0   0       0]);
+a2=2;a3=0.5;d2=4;d3=1;d4=2;
+% L(1)=Link([0	5	-pi/2 	pi/2   0]);
+% L(2)=Link([5	4	0       0   0]);
+% L(3)=Link([0	0	pi/2    pi/2   0]);
+% L(4)=Link([4	0	-pi/2	0	0]);
+% L(5)=Link([0	0	pi/2	0	0]);
+% L(6)=Link([0.5	0	0       0   0]);
+qli=[-pi        pi;...          %theta 1
+    -5*pi/4     pi/8;...        %theta 2
+    -pi/4       pi/4;...      %theta 3
+    -11*pi/18   17*pi/18;...    %theta 4
+    -5*pi/9     5*pi/9;...      %theta 5
+    -53*pi/36   53*pi/36];      %theta 6
+qz=[0 0 0 0 0 0];%matriz con vectores articulares rengl?n
+qfinal=[0 0  0 0 0 0];
+antropom=SerialLink(L,'name','antrop','qlim',qli); %construcci?n del objeto polar,
+%antropom.plotopt = {'workspace', [-9 9 -9 9 0 14]}; %opcion del m?todo plot,
+antropom.plotopt = {'workspace', [-6 6 -6 6 0 8]}; %opcion del m?todo plot,
+%Busquen los m?todos de SerialLink, en particular busquen
+% fkine, plot, teach, ikine, mtraj, ctraj, fdyn
+antropom.teach(qz)
+%T=antropom.fkine(qfinal)
+%XYZ=[path ones([size(path,1) 1])];
+%q=interp(XYZ,2);
+%rpp.plot(q);
---
+++ Actividad4/inversa.m
@@ -0,0 +1,78 @@
+function q = inversa_rpp(d_03)
+% documentacion
+
+dx=d_03(1);
+dy=d_03(2);
+dz=d_03(3);
+if(dy==0)
+    dy=10^-10;
+end
+a1=2;
+a2=2;
+d1=0;
+d2=0.5;
+d3=0.5;
+
+%%%%%%%%%%%%%
+ox=0;
+yc=0;
+d6=d3;
+xc=ox-d6*r13;
+yc=oy-d6*r23;
+d_2=a1+a2;
+q1 = atan2(xc,yc)-atan2(-sqrt(xc^2+yc^2-d_2^2),d_2);
+q2 = 
+% %%%%%%%%%%
+% d_2
+% zc
+% d1
+% a2
+% a3
+% s3
+% c1
+% c23
+% q1 = atan2(dx,dy);
+% q2 = atan2(-sqrt(dx^2+dy^2-d_2^2),zc-d1) - atan2(a2+a3,a3*s3);
+% D = (dx^2+dy^2-d+(zc-d1)^2-a2^2-a3^3)/(2*a2*a3);
+% q3 = atan2(D,+sqrt(1-D^2));
+% q4 = atan2(c1*c23*r13+s1*c23*r23+s23*r33,-c1*s23*r13-s1*s23*r23+c23*r33);
+% q5 = atan2(-s1*r11+c1*r21,s1*r12-c1*r22);
+% q6 = atan2(-s1*r11+c1*r21,s1*r12-c1*r22);
+%%%%%%%%%%%%
+% Th=atan2(la,lb);
+% l3=sqrt(la^2+lb^2);
+% 
+% q1=atan2(dy,dx);
+% 
+% X=(dx^2+dy^2+(dz-l1)^2-l2^2-l3^2)/(2*l2*l3);
+% q3=atan2(-sqrt(1-X^2),X)+Th;
+% 
+% A=(l2.^2+l3.^2+2.*l2.*l3.*cos(q3+(-1).*Th)).^(-1).*((dz+(-1).*l1).*l2+(dz+(-1).*l1).*l3.*cos(q3+(-1).*Th)+(-1).*dy.*l3.*csc(q1).*sin(q3+(-1).*Th));
+% B=(l2.^2+l3.^2+2.*l2.*l3.*cos(q3+(-1).*Th)).^(-1).*(dy.*(l2+l3.*cos(q3+(-1).*Th)).*csc(q1)+(dz+(-1).*l1).*l3.*sin(q3+(-1).*Th));
+% 
+% q2=atan2(A,B);
+
+% lol=l2.*cos(q1).*cos(q2)+l3.*(cos(q1).*cos(q2).*cos(q3+(-1).*Th)+(-1).*cos(q1).*sin(q2).*sin(q3+(-1).*Th))
+%     
+% if(dx==l2.*cos(q1).*cos(q2)+l3.*(cos(q1).*cos(q2).*cos(q3+(-1).*Th)+(-1).*cos(q1).*sin(q2).*sin(q3+(-1).*Th)))
+%     dx
+%     l2.*cos(q1).*cos(q2)+l3.*(cos(q1).*cos(q2).*cos(q3+(-1).*Th)+(-1).*cos(q1).*sin(q2).*sin(q3+(-1).*Th))
+% end
+% if(dy==l2.*cos(q2).*sin(q1)+l3.*(cos(q2).*cos(q3+(-1).*Th).*sin(q1)+(-1).*sin(q1).*sin(q2).*sin(q3+(-1).*Th)))
+%     2
+% end
+% if(dz==l1+l2.*sin(q2)+l3.*(cos(q3+(-1).*Th).*sin(q2)+cos(q2).*sin(q3+(-1).*Th)))
+%     3
+% end
+
+
+
+
+q=[q1,q2,q3,q4,q5,q6];
+
+
+
+
+
+
+
