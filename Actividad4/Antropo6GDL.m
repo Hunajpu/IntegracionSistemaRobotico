3fd6852802c174cb01c6f4c9eff13ad1b1fa2dc7
Emiliano-Montero <81664197+Emiliano-Montero@users.noreply.github.com>
2021-08-29 01:04:42 -0500

    Primeros tres eslabones

--- Actividad4/Antropo6GDL.m
+++ Actividad4/Antropo6GDL.m
@@ -33,14 +33,14 @@ grid on
 %% Configuracion del robot
 %theta d a alpha sigma offset
 %l1a=7;l1b=2;l2=4;l3=4;l4=1.5;l5=1.5;l6=3;
-a1=5;a2=5;d1=7;d2=2;d3=1;
+a1=5;a2=5;d1=7;d2=0.4;d3=1;
 L(1)=Link([0        d1	0	-pi/2   0]);
 L(2)=Link([0        d2	a1  0       0]);
 L(3)=Link([0        0	a2  0       0]);
-L(4)=Link([-pi/2    0	0	-pi/2   0]);
-L(5)=Link([pi/2     0	0	pi/2    0]);
-L(6)=Link([0        d3	0   0       0]);
-a2=2;a3=0.5;d2=4;d3=1;d4=2;
+% L(4)=Link([-pi/2    0	0	-pi/2   0]);
+% L(5)=Link([pi/2     0	0	pi/2    0]);
+% L(6)=Link([0        d3 0   0   0]);
+% a2=2;a3=0.5;d2=4;d3=1;d4=2;
 % L(1)=Link([0	5	-pi/2 	pi/2   0]);
 % L(2)=Link([5	4	0       0   0]);
 % L(3)=Link([0	0	pi/2    pi/2   0]);
@@ -49,19 +49,34 @@ a2=2;a3=0.5;d2=4;d3=1;d4=2;
 % L(6)=Link([0.5	0	0       0   0]);
 qli=[-pi        pi;...          %theta 1
     -5*pi/4     pi/8;...        %theta 2
-    -pi/4       pi/4;...      %theta 3
-    -11*pi/18   17*pi/18;...    %theta 4
-    -5*pi/9     5*pi/9;...      %theta 5
-    -53*pi/36   53*pi/36];      %theta 6
-qz=[0 0 0 0 0 0];%matriz con vectores articulares rengl?n
-qfinal=[0 0  0 0 0 0];
+    -pi/4       pi/4];%...      %theta 3
+%     -11*pi/18   17*pi/18;...    %theta 4
+%     -5*pi/9     5*pi/9;...      %theta 5
+%     -53*pi/36   53*pi/36];      %theta 6
+qz=[0 0 0];%matriz con vectores articulares rengl?n
+qfinal=[0 0 0];
 antropom=SerialLink(L,'name','antrop','qlim',qli); %construcci?n del objeto polar,
 %antropom.plotopt = {'workspace', [-9 9 -9 9 0 14]}; %opcion del m?todo plot,
-antropom.plotopt = {'workspace', [-6 6 -6 6 0 8]}; %opcion del m?todo plot,
+antropom.plotopt = {'workspace', [-9 9 -9 9 0 14]}; %opcion del m?todo plot,
 %Busquen los m?todos de SerialLink, en particular busquen
 % fkine, plot, teach, ikine, mtraj, ctraj, fdyn
-antropom.teach(qz)
-% T=antropom.fkine(qfinal)
-% XYZ=[path ones([size(path,1) 1])];
-% q=interp(XYZ,2);
-%rpp.plot(q);
+%antropom.teach(qz)
+%T=antropom.fkine(qfinal)
+XYZ=[path ones([size(path,1) 1])];
+q=interp(XYZ,2);
+
+%% Graficación de Trayectoria Real
+hold on
+trayectory=zeros(size(q,1),3);
+for i=1:1:size(q,1)
+    q1=q(i,1);
+    q2=q(i,2);
+    q3=q(i,3);
+    q4=0;%q(i,4);
+    q5=0;%q(i,5);
+    q6=0;%q(i,6);
+    trayectory(i,1:3)=[d3*(sin(q1)*sin(q5) + sin(q2 + q3 + q4)*cos(q1)*cos(q5)) - d2*sin(q1) + a1*cos(q1)*cos(q2) + a2*cos(q1)*cos(q2)*cos(q3) - a2*cos(q1)*sin(q2)*sin(q3); d2*cos(q1) + a1*cos(q2)*sin(q1) - d3*cos(q1)*sin(q5) + a2*cos(q2)*cos(q3)*sin(q1) - a2*sin(q1)*sin(q2)*sin(q3) + d3*sin(q2 + q3 + q4)*cos(q5)*sin(q1); d1 - a2*sin(q2 + q3) - a1*sin(q2) + d3*cos(q2 + q3 + q4)*cos(q5)];
+end
+plot3(trayectory(:,1), trayectory(:,2), trayectory(:,3), 'color', 'b', 'LineWidth', 2)
+
+antropom.plot(q);
